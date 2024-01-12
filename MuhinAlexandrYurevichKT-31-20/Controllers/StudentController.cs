9dce21217dd2f084e2464d484066da92c3dad60d
muhin-alexander-yurevich-kt-31-20 <ds50323@yandex.ru>
2024-01-12 05:00:24 +0300

    feat: add new methods

--- MuhinAlexandrYurevichKT-31-20/Controllers/StudentController.cs
+++ MuhinAlexandrYurevichKT-31-20/Controllers/StudentController.cs
@@ -14,18 +14,72 @@ namespace MuhinAlexandrYurevichKT_31_20.Controllers
     {
         private readonly ILogger<StudentController> _logger;
         private readonly IStudentService _studentService;
+        public MuhinDbContext _dbcontext;
 
-        public StudentController(ILogger<StudentController> logger, IStudentService studentService)
+        public StudentController(ILogger<StudentController> logger, IStudentService studentService, MuhinDbContext context)
         {
             _logger = logger;
             _studentService = studentService;
+            _dbcontext = context;
         }
-        [HttpPost(Name = "GetStudentsBuGroup")]
+
+        [HttpPost(Name = "GetStudentsByGroup")]
         public async Task<IActionResult> GetStudentsByGroupAsync(StudentGroupFilter filter, CancellationToken cancellationToken)
         {
-
             var students = await _studentService.GetStudentsByGroupAsync(filter, cancellationToken);
             return Ok(students);
+        }
+
+        [HttpPost("AddStudent", Name = "AddStudent")]
+        public IActionResult CreateStudent([FromBody] StudentAddFilter filter)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            var student = new Student();
+            student.FirstName = filter.FirstName;
+            student.LastName = filter.LastName;
+            student.MiddleName = filter.MiddleName;
+            student.GroupId = _dbcontext.Set<Models.Group>().FirstOrDefault(g => g.GroupId == filter.GroupId).GroupId;
+
+            _dbcontext.Set<Student>().Add(student);
+            _dbcontext.SaveChanges();
+            return Ok(student);
+        }
+
+        [HttpPut("EditStudent")]
+        public IActionResult UpdateStudent(int id, [FromBody] StudentAddFilter filter)
+        {
+            var existingStudent = _dbcontext.Set<Student>().FirstOrDefault(g => g.StudentId == id);
+
+            if (existingStudent == null)
+            {
+                return NotFound();
+            }
+
+            existingStudent.FirstName = filter.FirstName;
+            existingStudent.LastName = filter.LastName;
+            existingStudent.MiddleName = filter.MiddleName;
+            existingStudent.GroupId = _dbcontext.Set<Models.Group>().FirstOrDefault(g => g.GroupId == filter.GroupId).GroupId;
+            _dbcontext.SaveChanges();
+
+            return Ok();
+        }
+
+        [HttpDelete("DeleteStudent")]
+        public IActionResult DeleteGroup(int id)
+        {
+            var existingStudent = _dbcontext.Set<Student>().FirstOrDefault(g => g.StudentId == id);
+
+            if (existingStudent == null)
+            {
+                return NotFound();
+            }
+            _dbcontext.Set<Student>().Remove(existingStudent);
+            _dbcontext.SaveChanges();
+
+            return Ok();
         }
     }
 }
---
+++ MuhinAlexandrYurevichKT-31-20/Filters/StudentFilters/StudentsAddFilter.cs
@@ -0,0 +1,10 @@
+﻿namespace MuhinAlexandrYurevichKT_31_20.Filters.StudentFilters
+{
+    public class StudentAddFilter
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string MiddleName { get; set; }
+        public int GroupId { get; set; }
+    }
+}
