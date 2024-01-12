632705f6453492e308bca661cb1be4bab5d83c52
muhin-alexander-yurevich-kt-31-20 <ds50323@yandex.ru>
2024-01-12 03:18:49 +0300

    feat: add dbcontet and CreateDatabase migration

---
+++ MuhinAlexandrYurevichKT-31-20/Database/MuhinDbContext.cs
@@ -0,0 +1,24 @@
+﻿using MuhinAlexandrYurevichKT_31_20.Database.Configurations;
+using MuhinAlexandrYurevichKT_31_20.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace MuhinAlexandrYurevichKT_31_20.Database
+{
+    public class MuhinDbContext : DbContext
+    {
+        DbSet<Student> Students { get; set; }
+        DbSet<Group> Groups { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Конфигурации к таблицам
+            modelBuilder.ApplyConfiguration(new StudentConfiguration());
+            modelBuilder.ApplyConfiguration(new GroupConfiguration());
+
+        }
+
+        public MuhinDbContext(DbContextOptions<MuhinDbContext> options) : base(options)
+        {
+        }
+    }
+}
--- MuhinAlexandrYurevichKT-31-20/Program.cs
+++ MuhinAlexandrYurevichKT-31-20/Program.cs
@@ -1,5 +1,7 @@
 using NLog;
 using NLog.Web;
+using MuhinAlexandrYurevichKT_31_20.Database;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -15,6 +17,11 @@ try
     // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
     builder.Services.AddEndpointsApiExplorer();
     builder.Services.AddSwaggerGen();
+
+    // Контекст БД
+    var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+    builder.Services.AddDbContext<MuhinDbContext>(options =>
+        options.UseSqlServer(connectionString));
 
     var app = builder.Build();
 
